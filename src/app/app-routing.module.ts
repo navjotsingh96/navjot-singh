c52393ee4d563af62152bc023f2be479b37705de
navjotsingh96 <navjot.singh96@web.de>
2022-04-15 11:11:24 +0200

    add route but with a bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
-const routes: Routes = [];
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'my-work', component: MyWorkComponent},
+  {path: 'about-me', component: AboutMeComponent},
+  {path: 'contact-me', component: ContactMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
