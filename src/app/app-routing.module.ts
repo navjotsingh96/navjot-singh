13acb678209e228989a997fedee0450f9034ab16
navjotsingh96 <navjot.singh96@web.de>
2022-04-16 13:21:27 +0200

    added header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SkillsComponent } from './skills/skills.component';
 import { MyWorkComponent } from './my-work/my-work.component';
@@ -8,11 +8,31 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
-  {path: 'skills', component: SkillsComponent},
+  {path: '', component: MainComponent,
+  outlet:'main',
+
+  children: [
+  
+    {
+        path: '',
+        component: SkillsComponent,
+        outlet: 'skills'
+    },
+    {
+        path: '',
+        component: MyWorkComponent,
+        outlet: 'aboutme'
+    },
+    {
+      path: '',
+      component: ContactMeComponent,
+      outlet: 'contact'
+  }
+]},
+ /*  {path: 'skills', component: SkillsComponent},
   {path: 'my-work', component: MyWorkComponent},
   {path: 'about-me', component: AboutMeComponent},
-  {path: 'contact-me', component: ContactMeComponent}
+  {path: 'contact-me', component: ContactMeComponent} */
 ];
 
 @NgModule({
