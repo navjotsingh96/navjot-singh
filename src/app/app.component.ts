b8f4629101b163d1ced82e1abab5a5a5dfa0b760
navjotsingh96 <navjot.singh96@web.de>
2022-05-11 11:12:18 +0200

    fixed height bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @Input() darkMode= false;
+ 
   constructor(public router : Router){
 
 
